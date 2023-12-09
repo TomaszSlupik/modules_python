1bc0239d1b320ac09fcf3948be01170137a65ddc
TomaszSlupik <slupiktomasz@gmail.com>
2023-12-09 08:41:04 +0100

    module_sys

--- module_sys.py
+++ module_sys.py
@@ -1,1 +1,18 @@
-import sys
+import sys 
+
+# ścieżka dla pliku binarnego
+print(sys.executable)
+
+print("---")
+
+# Lista Modułów
+print(sys.path)
+
+print("---")
+
+# przekazane argumenty
+for arg in sys.argv[1:]:
+    print (arg)
+
+print("---")
+
