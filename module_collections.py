dea262a7e94676afe9b8d4b3bcb1f2c81bb60a04
TomaszSlupik <slupiktomasz@gmail.com>
2023-12-12 08:15:46 +0100

    module_collections

--- module_collections.py
+++ module_collections.py
@@ -87,3 +87,158 @@ count_slow_list_textChange = collections.Counter(slow_list_textChange)
 print(count_slow_list_textChange.most_common(3))
 
 print('---')
+
+# Zliczenie wystąpień zakończeń png i jpg
+
+fnames = [
+    '001_image.png',
+    '002_image.png',
+    '003_image.jpg',
+    '004_image.png',
+    '005_image.png',
+    '006_image.png',
+    '007_image.jpg',
+    '008_image.png',
+    '009_image.jpg',
+    '010_image.jpg',
+    '011_image.jpg',
+    '012_image.png',
+    '013_image.jpg',
+    '014_image.jpg',
+    '015_image.jpg',
+    '016_image.png',
+    '017_image.jpg',
+    '018_image.jpg',
+    '019_image.png',
+    '020_image.png',
+    '021_image.jpg',
+    '022_image.jpg',
+    '023_image.png',
+    '024_image.png',
+    '025_image.jpg',
+    '026_image.png',
+    '027_image.png',
+    '028_image.jpg',
+    '029_image.png',
+    '030_image.png',
+]
+
+finish_fnames = []
+
+for name in fnames:
+    finish_fnames.append(name[10:])
+
+count_finish_fnames = collections.Counter(finish_fnames)
+
+print(count_finish_fnames)
+
+print('---')
+
+# Złączenie obu słowników => ChainMap
+
+stocks_1 = {'CDR': 400, 'PLW': 490}
+stocks_2 = {'PLW': 500, 'TEN': 550, 'BBT': 30}
+
+stocks = collections.ChainMap(stocks_1, stocks_2)
+
+print(stocks)
+
+print('---')
+
+# Złączenie obu słowników + Znajdź wartość dla klucza 'PLW'
+
+stocks_3 = {'CDR': 400, 'PLW': 490}
+stocks_4 = {'PLW': 500, 'TEN': 550, 'BBT': 30}
+
+stocks_final = collections.ChainMap(stocks_3, stocks_4)
+
+stocks_final_count = collections.Counter(stocks_final)
+
+print(stocks_final_count['PLW'])
+
+print('---')
+
+#  Znajdź wartość dla klucza 'Samsung' w utworzonym obiekcie ChainMap
+techs = {'Apple': 370, 'Samsung': 1100, 'Microsoft': 201}
+finance = {'Citigroup': 51, 'Allianz': 180}
+gaming = {'Sony': 76, 'Nintendo': 470, 'EA': 135}
+
+sum_three = collections.ChainMap(techs, finance, gaming)
+
+print(sum_three['Samsung'])
+
+print('---')
+
+# zaktualizuj wartość dla klucza 'Microsoft' w słowniku techs na 210
+
+techs_second = {'Apple': 370, 'Samsung': 1100, 'Microsoft': 201}
+finance_second = {'Citigroup': 51, 'Allianz': 180}
+gaming_second = {'Sony': 76, 'Nintendo': 470, 'EA': 135}
+
+sum_second = collections.ChainMap(techs_second, finance_second, gaming_second)
+
+sum_second['Microsoft'] = 210
+
+print(sum_second['Microsoft'])
+
+print('---')
+
+# wyświetl listę wszystkich kluczy obiektu ChainMap do konsoli posortowanych alfabetycznie.
+
+techs_third = {'Apple': 370, 'Samsung': 1100, 'Microsoft': 201}
+finance_third = {'Citigroup': 51, 'Allianz': 180}
+gaming_third = {'Sony': 76, 'Nintendo': 470, 'EA': 135}
+
+stocks_third = collections.ChainMap(techs_third, finance_third, gaming_third)
+
+stocks_third_sort = sorted(stocks_third.keys())
+
+print(stocks_third_sort)
+
+print('---')
+
+# przykład z wartościami 
+
+class Car: 
+    def __init__(self, mode='eco', power_level=7):
+        self.mode = mode
+        self.power_level = power_level
+
+    def __str__(self):
+        return f"{self.mode, self.power_level}"
+
+carOne = Car()
+carTwo = Car('sport', 10)
+
+print(carTwo.mode)
+
+print('---')
+
+# moduł wbudowany collections oraz funkcję namedtuple(), zbuduj klasę o nazwie Point do przechowywania punktów na płaszczyźnie
+
+Point = collections.namedtuple(typename='Point', field_names=['x', 'y'])
+
+pointOne = Point(3, 4)
+pointTwo = Point(-2, 6)
+
+print(pointOne)
+print(pointTwo)
+
+print('---')
+
+# Znajdź punkt będący środkiem odcinka o końcach w punktach pt1 oraz pt2
+
+PointTwo = collections.namedtuple(typename='PointTwo', field_names=['x', 'y'])
+
+pt1 = PointTwo(3, 4)
+pt2 = PointTwo(-2, 6)
+
+firstX = (pt1.x + pt2.x) / 2 
+firstY = (pt1.y + pt2.y) / 2
+
+final = PointTwo( firstX, firstY)
+print (final)
+
+print('---')
+
+# 
